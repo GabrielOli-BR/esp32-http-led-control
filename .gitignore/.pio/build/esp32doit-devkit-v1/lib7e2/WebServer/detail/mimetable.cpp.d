25f74bce8380cfee962afae8cac24c3be9794596
Gabriel Oliveira - 82429936 <gabriel.oliveira.silvi@gmail.com>
2026-05-28 23:13:21 -0300

    gitignore

--- .gitignore/.pio/build/esp32doit-devkit-v1/lib7e2/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32doit-devkit-v1/lib7e2/WebServer/detail/mimetable.cpp.o: \
- C:/Users/gabri/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- C:/Users/gabri/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- C:/Users/gabri/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
